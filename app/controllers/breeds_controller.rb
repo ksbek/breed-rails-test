f36f7ce10372a3a79bd7df69b7c44ee3845f8b99
ksbek <ksbek112@yandex.com>
2018-07-03 06:22:40 +0400

    Pick a breed and see picture of that dog

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,7 +1,15 @@
 class BreedsController < ApplicationController
 
   def index
-    @breed = DogBreedFetcher.fetch
-  end
+    @breed_name = 'affenpinscher'
+    if params['breed']
+      @breed_name = params['breed']
+    end
 
+    @dog_url = ''
+    begin
+      @dog_url = JSON.parse(RestClient.get("https://dog.ceo/api/breed/#{@breed_name.gsub('-', '/')}/images/random").body)['message']
+    rescue Object => e
+    end
+  end
 end
--- app/helpers/breeds_helper.rb
+++ app/helpers/breeds_helper.rb
@@ -1,2 +1,18 @@
 module BreedsHelper
+  def breed_list
+    begin
+      breeds_list = JSON.parse(RestClient.get("https://dog.ceo/api/breeds/list/all").body)["message"]
+      breeds = {}
+      breeds_list.keys.each do |key|
+      	breeds[key.capitalize] = key
+      	breeds_list[key].each do |sub_breed|
+      		breed = [key, sub_breed].join('-')
+      		breeds[breed.titleize] = breed
+      	end
+      end
+      breeds
+    rescue Object => e
+      []
+    end
+  end
 end
